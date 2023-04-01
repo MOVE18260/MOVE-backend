c78cfeda05c71c4747aed36ed605735b4d0a285f
49EHyeon42 <49ehyeon42@gmail.com>
2023-04-01 18:52:55 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/dev/ehyeon/move/MoveApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.move;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
