5c0f8211cc9ce7a4a3ed6d29c1bf92cff14568d6
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-05-27 21:52:46 +0900

    fix: data.sql member id 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO member
-VALUES(1, '2000-01-01', 'test1@gmail.com', 'test1', 'password1', 'province1', '남자');
+VALUES(100, '2000-01-01', 'test1@gmail.com', 'test1', 'password1', 'province1', '남자');
 
 INSERT INTO member
-VALUES(2, '2002-02-02', 'test2@gmail.com', 'test2', 'password2', 'province2', '여자');
+VALUES(200, '2002-02-02', 'test2@gmail.com', 'test2', 'password2', 'province2', '여자');
 
 INSERT INTO member
-VALUES(3, '2003-03-03', 'test3@gmail.com', 'test3', 'password3', 'province3', '남자');
+VALUES(300, '2003-03-03', 'test3@gmail.com', 'test3', 'password3', 'province3', '남자');
 
 INSERT INTO member
-VALUES(4, '2004-04-04', 'test4@gmail.com', 'test4', 'password4', 'province4', '여자');
+VALUES(400, '2004-04-04', 'test4@gmail.com', 'test4', 'password4', 'province4', '여자');
 
 INSERT INTO member
-VALUES(5, '2005-05-05', 'test5@gmail.com', 'test5', 'password5', 'province5', '남자');
+VALUES(500, '2005-05-05', 'test5@gmail.com', 'test5', 'password5', 'province5', '남자');
